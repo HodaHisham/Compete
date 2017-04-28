1827b9d8bf78f9e68d9a113ea26819372578341d
HodaHisham <hodahisham@gmail.com>
2017-04-28 20:53:17 +0200

    testing database and adding some routes

--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,73 @@
 var express = require('express');
-
-var router = express.Router();
+var User    = require('../models/users');
+var router  = express.Router();
 
 // router.get('/', function(req, res){
 //   res.json({ message: 'hooray! welcome to our api!' });
 // });
 
-router.get('*', function(req, res) {
+router.get('/', function(req, res) {
       res.sendfile('./views/index.html');
+});
+
+router.post('/user', function(req, res) {
+
+        var user = new User();
+        user.id = req.body.id;
+        user.handle = req.body.handle;
+
+        user.save(function(err) {
+            if (err)
+                console.log(err);
+            else
+                res.json({ message: 'User created!' });
+        });
+
+});
+router.get('/user', function(req, res) {
+  User.find(function(err, users) {
+      if (err)
+          res.send(err);
+      else
+          res.json(users);
+  });
+});
+
+router.get('/user/:user_id', function(req, res) {
+  User.find({ id : req.params.user_id } , function(err, user) {
+            if (err)
+                res.send(err);
+            else
+                res.json(user);
+  });
+});
+
+router.delete('/user/:user_id', function(req, res) {
+  User.remove({ id : req.params.user_id } , function(err, user) {
+            if (err)
+                res.send(err);
+            else
+                res.json(user);
+  });
+});
+
+router.put('/user/:user_id', function(req, res) {
+  User.find({ id : req.params.user_id } , function(err, user) {
+            if (err)
+                res.send(err);
+            else
+            {
+              user.id = req.body.id;
+              user.handle = req.body.handle;
+
+              user.save(function(err) {
+                  if (err)
+                      console.log(err);
+                  else
+                      res.json({ message: 'User updated!' });
+              });
+            }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // PACKAGES SET UP
 var express = require('express');
-var app     = express();
 var bodyParser = require('body-parser');
-var mongoose = require('./db');
-var user = require('./models/users');
+var mongoose = require('mongoose');
+var db = require('./db');
 var router = require('./routes/route');
+var app     = express();
 
 // CONFIGURE APP TO USE bodyparser
 app.use(bodyParser.urlencoded( {extended: true }));
 app.use(bodyParser.json());
+
+// CONNECT DATABASE
+mongoose.connect(db.url);
 
 // SET PORT
 var port = process.env.PORT || 8080;
