0f13b66a43c2b2c970806e2a6123a8fc255401db
MonaHashem <mona.mo.hashem@gmail.com>
2017-04-29 00:11:45 +0200

    some changes to user.schema

--- models/users.js
+++ models/users.js
@@ -2,11 +2,20 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var UserSchema   = new Schema({
-    id      : {
+    id : {
       type: String,
       required : true
     },
-    handle  : String
+    name: String,
+    handle: String,
+    div1: Boolean,
+    div2: Boolean,
+    other: Boolean,
+    gym: Boolean,
+    eng: Boolean,
+    russ: Boolean,
+    rated: Boolean,
+    unrated:Boolean
 });
 
 module.exports = mongoose.model('User', UserSchema);
